e254b1fb5de84750d9745eb6b05d1b4276c4510c
Xueer <snowlily14@gmail.com>
2019-04-30 16:12:27 -0400

    Add web content and layout

---
+++ app/src/main/java/com/buddy/activity/WebBrowserActivity.java
@@ -0,0 +1,23 @@
+package com.buddy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.buddy.main.R;
+
+public class WebBrowserActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_browser);
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+    }
+}
