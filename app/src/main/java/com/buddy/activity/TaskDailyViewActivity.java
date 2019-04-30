73cf6b4a88de786248f47a0ddebdcd4eb005bd9b
Xueer <snowlily14@gmail.com>
2019-04-30 16:12:27 -0400

    Add web content and layout

---
+++ app/src/main/java/com/buddy/activity/TaskDailyViewActivity.java
@@ -0,0 +1,15 @@
+package com.buddy.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.buddy.main.R;
+
+public class TaskDailyViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_by_day);
+    }
+}
