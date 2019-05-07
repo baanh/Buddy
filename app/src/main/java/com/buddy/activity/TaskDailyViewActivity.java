33ac3ffc9f9f5130a9d84141148d1af3779fe012
Luke Nguyen <baanhnguyen92@gmail.com>
2019-05-07 01:29:40 -0400

    Add audio, fix expandable list

--- app/src/main/java/com/buddy/activity/TaskDailyViewActivity.java
+++ app/src/main/java/com/buddy/activity/TaskDailyViewActivity.java
@@ -1,126 +1,109 @@
 package com.buddy.activity;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-import android.arch.lifecycle.Observer;
+import android.annotation.SuppressLint;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
+import android.widget.TextView;
 
 import com.buddy.adapters.TaskExpandableListAdapter;
 import com.buddy.entity.Task;
 import com.buddy.main.R;
 import com.buddy.viewmodel.TaskViewModel;
 
-import android.widget.EditText;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
-import android.util.Log;
-
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
-public class TaskDailyViewActivity extends AppCompatActivity {
+public class TaskDailyViewActivity extends AppCompatActivity
+        implements TextToSpeech.OnInitListener {
 
-    private ExpandableListView taskListView;
-    private ExpandableListAdapter listAdapter;
     private List<String> listDataHeader;
     private HashMap<String, List<String>> listHash;
     private TaskViewModel taskViewModel;
-  
-    private EditText taskText;
-    private ArrayList<String> tasks = new ArrayList<String>();
+    private TextView txtTodayDate;
 
     private TextToSpeech speaker;
-    private static final String tag = "Widgets";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_by_day);
-      
+
         //Initialize text to speech engine
-        speaker = new TextToSpeech(this, (OnInitListener) this);
+        speaker = new TextToSpeech(getApplicationContext(), this);
+        txtTodayDate = findViewById(R.id.txt_today_date);
 
         taskViewModel = ViewModelProviders.of(this).get(TaskViewModel.class);
         listDataHeader = new ArrayList<>();
         listHash = new HashMap<>();
-        taskViewModel.getAllTasks().observe(this, new Observer<List<Task>>() {
-            @Override
-            public void onChanged(@Nullable List<Task> tasks) {
-                if (tasks != null) {
-                    for (Task task : tasks) {
-                        List<String> taskDetail = new ArrayList<>();
-                        listDataHeader.add(task.getName());
-                        taskDetail.add("Start: " + task.getStartDate().toString());
-                        taskDetail.add("End: " + task.getEndDate().toString());
-                        taskDetail.add("Description: " + task.getDescription());
-                        listHash.put(listDataHeader.get(listDataHeader.size() - 1), taskDetail);
-                    }
-                }
+
+        // Find tasks between today and tomorrow
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter
+                = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date mToday = formatter.parse(formatter.format(new Date()));
+            Date mTomorrow = new Date(mToday.getTime() + (1000 * 60 * 60 * 24) - 1);
+
+            txtTodayDate.setText(formatter.format(new Date()));
+            List<Task> tasks = taskViewModel.findTasksBetweenDate(mToday, mTomorrow);
+            for (Task task : tasks) {
+                List<String> taskDetail = new ArrayList<>();
+                listDataHeader.add(task.getName());
+                taskDetail.add("Start: " + task.getStartDate().toString());
+                taskDetail.add("End: " + task.getEndDate().toString());
+                taskDetail.add("Description: " + task.getDescription());
+                listHash.put(listDataHeader.get(listDataHeader.size() - 1), taskDetail);
             }
-        });
-        taskListView = findViewById(R.id.task_expandable_list_view);
-        listAdapter = new TaskExpandableListAdapter(this, listDataHeader, listHash);
+            speakTodayTasks(tasks);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        ExpandableListView taskListView = findViewById(R.id.task_expandable_list_view);
+        ExpandableListAdapter listAdapter = new TaskExpandableListAdapter(this, listDataHeader, listHash);
         taskListView.setAdapter(listAdapter);
     }
-    
-    //speaks the contents of output
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP) //have no idea what this is but will show error without this line
-    public void speak(String output){
-        //	speaker.speak(output, TextToSpeech.QUEUE_FLUSH, null);  //for APIs before 21
-        speaker.speak(output, TextToSpeech.QUEUE_FLUSH, null, "Id 0");
+
+    // @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void speak(String output) {
+        speaker.speak(output, TextToSpeech.QUEUE_FLUSH, null);
     }
 
-    // Implements TextToSpeech.OnInitListener.
+    @Override
     public void onInit(int status) {
-        // status can be either TextToSpeech.SUCCESS or TextToSpeech.ERROR.
-        if (status == TextToSpeech.SUCCESS) {
-            // Set preferred language to US english.
-            // If a language is not be available, the result will indicate it.
-            int result = speaker.setLanguage(Locale.US);
-
-            //int result = speaker.setLanguage(Locale.FRANCE);
-            if (result == TextToSpeech.LANG_MISSING_DATA ||
-                    result == TextToSpeech.LANG_NOT_SUPPORTED) {
-                // Language data is missing or the language is not supported.
-                Log.e(tag, "Language is not available.");
-            } else {
-                // The TTS engine has been successfully initialized
-                speak("Please enter your bill amount");
-                Log.i(tag, "TTS Initialization successful.");
-            }
-        } else {
-            // Initialization failed.
-            Log.e(tag, "Could not initialize TextToSpeech.");
+        if (status != TextToSpeech.ERROR) {
+            speaker.setLanguage(Locale.US);
         }
     }
 
-    // on destroy
-    public void onDestroy(){
-
-        // shut down TTS engine
-        if(speaker != null){
+    public void onPause() {
+        if (speaker != null) {
             speaker.stop();
             speaker.shutdown();
         }
-        super.onDestroy();
+        super.onPause();
     }
 
-    public void getTodayTasks(ArrayList tasks){
+    public void speakTodayTasks(List<Task> tasks) {
         int num = tasks.size();
-        speak("Hi Buddy, today you have "+num+"events.");
-        for (int i=0; i<tasks.size(); i++){
-            String taskName = tasks.get(i).toString();
-            speak(taskName);
-            /*If we also want it to announce the start and end time of each task,
-            Might need a HashMap structure to store the task and its corresponding times,
-            Not sure what form should the start time and end time be stored and read from*/
-        }
+        speaker.speak("Hi Buddy, today you have events.", TextToSpeech.QUEUE_FLUSH, null);
+//        for (Task task : tasks) {
+//            String taskName = task.getName();
+//            speak(taskName);
+//            /*If we also want it to announce the start and end time of each task,
+//            Might need a HashMap structure to store the task and its corresponding times,
+//            Not sure what form should the start time and end time be stored and read from*/
+//        }
     }
 }
